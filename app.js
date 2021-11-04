db657dc4f5f48721e728621d8228cdd3ebeef83e
Musa Mutetwi Muliro <mmuliro@safaricom.co.ke>
2021-11-05 02:41:27 +0300

    Completed all functionalities

--- app.js
+++ app.js
@@ -1,6 +1,17 @@
-// Import libraries
+/* 
+Project: Tic Tac Toe
+ * Date: 03/11/2021
+ * Author: Musa Mutetwi
+ * Description: This is a Tic Tac Toe game
+ * Score:
+ *  1️⃣ Your api implements the specified interface: ✔️
+ *  2️⃣ Your api rejects invalid boards and accepts valid boards: ✔️ (app.js Line 30)
+ *  3️⃣ Your code is readable, easy to understand and short: 🙏
+ *  4️⃣ You successfully implement the strategy above (do not implement Minimax): ✔️
+ */
+
+// Import express
 const express = require("express");
-const cors = require("cors");
 
 // Invoke app
 const app = express();
@@ -8,16 +19,38 @@ const app = express();
 // Define PORT
 const PORT = process.env.PORT || "3333";
 
+// Get the request and grab the board parameter
+app.get("/", (req, res) => {
+  const _board = req.query.board;
+
+  // Check if the _board string is valid
+  const BoardIsValid = _board
+    .split("")
+    .every((value) => value === "x" || value === "o" || value === " ");
+
+  if (_board.length !== 9 || !BoardIsValid) {
+    res.status(400).send("Invalid board");
+  } else {
+    // Set header to plain text to prevent space trimming
+    res.setHeader("Content-Type", "text/plain");
+
+    // Create array from _board
+    const board = Array.from(_board);
+
+    // Import game logic
+    const game = require("./game");
+
+    // Invoke game logic
+    res.send(game(board).toString().split(",").join(""));
+  }
+});
+
+// Capture 404s
+app.all("*", function (req, res) {
+  res.status(404).send("Page not found!");
+});
+
 // Listen
 app.listen(PORT, () => {
   console.log(`Listening at: ${PORT}`);
-});
-
-// Register Middlewares
-app.use(cors());
-app.use(express.urlencoded({ extended: false }));
-
-// Auto-process
-app.get("/", (req, res) => {
-  res.send("req");
 });
--- game.js
+++ game.js
@@ -1,207 +1,155 @@
-const readline = require("readline");
+const CurrentTurn = (board) => {
+  let x = 0,
+    o = 0;
 
-const TicTacToe = function (readline) {
-  let ReadLine = null,
-    BoardSize = 3,
-    BoardData = {},
-    PlayerMarkers = ["x", "o"],
-    players = [];
+  board.forEach((e) => {
+    if (e === "x") x++;
+    if (e === "o") o++;
+  });
 
-  const Initialize = (readline) =>
-    (ReadLine = readline.createInterface({
-      input: process.stdin,
-      output: process.stdout,
-    }));
-
-  Initialize(readline);
-
-  const CreateTheBoard = () => {
-    let data = {
-      board: [],
-      empty: [],
-    };
-
-    for (let i = 0; i < BoardSize; i += 1) {
-      let row = [];
-
-      for (let k = 0; k < BoardSize; k += 1) {
-        row.push(null);
-        data.empty.push(i + " " + k);
-      }
-
-      data.board.push(row);
-    }
-
-    BoardData = data;
-  };
-
-  const SetupPlayers = (PlayerTypes) => {
-    let InitialIndex = 0;
-
-    if (PlayerTypes.length === 2) {
-      InitialIndex = Math.round(Math.random());
-
-      players = [
-        {
-          marker: PlayerMarkers[InitialIndex],
-          type: PlayerTypes[0],
-        },
-        {
-          marker: PlayerMarkers[!InitialIndex | 0],
-          type: PlayerTypes[1],
-        },
-      ];
-    }
-  };
-
-  const ChangePlayers = () => {
-    players.push(players.shift());
-  };
-
-  const GetEachPlayerMove = () => {
-    let pos = [];
-    players[0].type === "server"
-      ? (pos = ServerMove(AddPlayerToTheBoard))
-      : (pos = GetPositionFromInput(AddPlayerToTheBoard));
-  };
-
-  const ServerMove = (callback) => {
-    let index = -1,
-      pos = [];
-
-    index = Math.floor(Math.random() * BoardData.empty.length - 0);
-    pos = GetPositionFromString(BoardData.empty[index]);
-
-    setTimeout(() => callback(pos), 1000);
-  };
-
-  const GetPositionFromInput = (callback) => {
-    ReadLine.question(
-      "Player " + players[0].marker + "'s move (enter row column): ",
-      function (input) {
-        BoardData.empty.indexOf(input) != -1
-          ? callback(GetPositionFromString(input))
-          : GetPositionFromInput(callback);
-      }
-    );
-  };
-
-  const GetPositionFromString = (str) => {
-    let pos = str.split(" ");
-    pos.forEach((el, i, arr) => (arr[i] = parseInt(el)));
-    return pos;
-  };
-
-  const AddPlayerToTheBoard = (pos) => {
-    AddToBoard(players[0], pos);
-
-    console.log("\u001b[2J\u001b[0;0H");
-    PrintBoard();
-
-    if (BoardData.empty.length == 0) {
-      console.log("Draw!");
-      ReadLine.close();
-      return;
-    }
-
-    if (CheckBoard(players[0])) {
-      ReadLine.close();
-      return;
-    } else {
-      ChangePlayers();
-      GetEachPlayerMove(players[0]);
-    }
-  };
-
-  const AddToBoard = (player, pos) => {
-    let EmptyIndex = -1;
-
-    // Add the player's position to the board
-    BoardData.board[pos[0]][pos[1]] = player.marker;
-
-    // Remove this position from the array of empty cells
-    EmptyIndex = BoardData.empty.indexOf(pos[0] + " " + pos[1]);
-    BoardData.empty.splice(EmptyIndex, 1);
-  };
-
-  const PrintBoard = () => {
-    let divider = "+-----------+";
-
-    for (let i = 0; i < 3; i += 1) {
-      console.log(divider);
-      let row = "|";
-
-      for (let k = 0; k < 3; k += 1) {
-        if (BoardData.board[i][k]) row += " " + BoardData.board[i][k] + " |";
-        else row += "   |";
-      }
-      console.log(row);
-    }
-    console.log(divider);
-  };
-
-  const CheckBoard = (player) => {
-    let board = BoardData.board,
-      PlayerString = player.marker + player.marker + player.marker,
-      winner = null,
-      ColumnStrings = ["", "", ""],
-      DiagonalStrings = ["", ""];
-
-    for (let i = 0; i < 3; i += 1) {
-      // Check each row for a winner
-      let RowString = board[i].join("");
-
-      if (RowString === PlayerString) {
-        winner = player;
-        break;
-      }
-
-      // Build a string of column values
-      for (var k = 0; k < 3; k += 1) {
-        ColumnStrings[k] += board[i][k];
-      }
-
-      // Build a string of diagonal values
-      if (i === 0) {
-        DiagonalStrings[0] += board[i][0];
-        DiagonalStrings[1] += board[i][2];
-      } else if (i == 1) {
-        DiagonalStrings[0] += board[i][1];
-        DiagonalStrings[1] += board[i][1];
-      } else if (i == 2) {
-        DiagonalStrings[0] += board[i][2];
-        DiagonalStrings[1] += board[i][0];
-      }
-    }
-
-    // Check the column strings for a winner
-    if (ColumnStrings.indexOf(PlayerString) > -1) {
-      winner = player;
-    }
-
-    // Check the diagonals for a winner
-    if (DiagonalStrings.indexOf(PlayerString) > -1) {
-      winner = player;
-    }
-
-    if (winner) {
-      console.log(winner.marker + " wins!");
-      return true;
-    }
-
-    return false;
-  };
-
-  return {
-    start: (PlayerTypes) => {
-      CreateTheBoard();
-      SetupPlayers(PlayerTypes);
-      players[0].marker !== "x" && ChangePlayers();
-      GetEachPlayerMove();
-    },
-    GetPlayers: () => players,
-  };
+  return x === o ? "x" : "o";
 };
 
-let ticTacToe = new TicTacToe(readline);
-ticTacToe.start(["me", "server"]);
+const AddBoard = (board, position) => {
+  let move = CurrentTurn(board);
+  let NewBoard = [];
+
+  for (let i = 0; i < board.length; i++) {
+    i === position ? NewBoard.push(move) : NewBoard.push(board[i]);
+  }
+
+  return NewBoard;
+};
+
+const FirstMove = (board) =>
+  board.indexOf("x") === 4 || board.indexOf("x") === -1 ? 0 : 4;
+
+const SecondMove = (board) => {
+  switch (board.indexOf("o")) {
+    case 1:
+    case 2:
+      return 3;
+      break;
+    case 3:
+    case 4:
+    case 6:
+      return 1;
+      break;
+    case 5:
+      return 4;
+      break;
+    case 7:
+    case 8:
+      return 2;
+      break;
+  }
+};
+
+const winningMove = (board) => {
+  var WinningCombinations = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+
+  var WinningIndex = false;
+
+  var move = WinningCombinations.find((combo) => {
+    let xCount = 0;
+    let yCount = 0;
+    let emptyCount = 0;
+    let emptyIndex;
+
+    combo.reduce((n, val, index) => {
+      if (board[val] === "x") xCount++;
+      if (board[val] === "o") yCount++;
+      if (board[val] === " ") {
+        emptyCount++;
+        emptyIndex = val;
+      }
+    }, 0);
+
+    if ((xCount || yCount) === 2 && emptyCount === 1) WinningIndex = emptyIndex;
+
+    return (xCount || yCount) === 2 && emptyCount === 1;
+  });
+
+  return WinningIndex;
+};
+
+const GameCounter = (board) => {
+  let counter = 0;
+
+  board.forEach((element) => {
+    if (element !== " ") counter++;
+  });
+
+  return counter;
+};
+
+const IncrementalMove = (board) => {
+  if (typeof winningMove(board) === "number") {
+    return winningMove(board);
+  }
+
+  switch (GameCounter(board)) {
+    case 3:
+      if (board[0] === "x" && board[5] === "x") return 7;
+      if (board[0] === "x" && board[7] === "x") return 5;
+      if (board[0] === "x" && board[8] === "x") return 1;
+      if (board[1] === "x" && board[3] === "x") return 2;
+      if (board[1] === "x" && board[5] === "x") return 0;
+      if (board[1] === "x" && board[6] === "x") return 5;
+      if (board[1] === "x" && board[7] === "x") return 0;
+      if (board[1] === "x" && board[8] === "x") return 3;
+      if (board[2] === "x" && board[3] === "x") return 7;
+      if (board[2] === "x" && board[6] === "x") return 1;
+      if (board[2] === "x" && board[7] === "x") return 3;
+      if (board[3] === "x" && board[5] === "x") return 0;
+      if (board[3] === "x" && board[7] === "x") return 0;
+      if (board[3] === "x" && board[8] === "x") return 1;
+      if (board[4] === "x" && board[8] === "x") return 2;
+      if (board[5] === "x" && board[6] === "x") return 1;
+      if (board[5] === "x" && board[7] === "x") return 2;
+      break;
+    case 4:
+      if (board[1] === "o" && board[6] === "o") return 4;
+      if (board[2] === "o" && board[3] === "o") return 4;
+      if (board[1] === "o" && board[8] === "o") return 6;
+      break;
+    default:
+      return board.indexOf(" ");
+  }
+};
+
+const move = (board) => {
+  switch (GameCounter(board)) {
+    case 0:
+    case 1:
+      return FirstMove(board);
+      break;
+    case 2:
+      return SecondMove(board);
+      break;
+    case 3:
+    case 4:
+    case 5:
+    case 6:
+    case 7:
+    case 8:
+      return IncrementalMove(board);
+      break;
+  }
+};
+
+const game = (board) => {
+  let position = move(board);
+  return AddBoard(board, position);
+};
+
+module.exports = game;
